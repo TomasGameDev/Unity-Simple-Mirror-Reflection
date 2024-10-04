dd1ab4419f2a2c633f8a5c4c2a81f90271b4f36b
TmasGameDev <93924533+TomasGameDev@users.noreply.github.com>
2024-10-04 20:37:09 +0300

    Initial commit

---
+++ Mirror.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Mirror : MonoBehaviour
+{
+    public Transform mainCameraTransform;
+    [Space]
+    public Camera mirrorCamera;
+    [Space]
+    public Transform mirrorCenter;
+
+    [Space]
+    public MeshRenderer surface;
+    public RenderTexture renderTexture;
+    public Vector2Int renderTextureSize = new Vector2Int(256, 256);
+
+    private void Update()
+    {
+        if (renderTexture == null)
+        {
+            renderTexture = new RenderTexture(renderTextureSize.x, renderTextureSize.y, 16);
+            renderTexture.name = "MirrorReflection" + GetInstanceID();
+            renderTexture.hideFlags = HideFlags.DontSave;
+
+            mirrorCamera.targetTexture = renderTexture;
+            surface.material.mainTexture = renderTexture;
+        }
+        Vector3 localPos = mirrorCenter.InverseTransformPoint(mainCameraTransform.position);
+        mirrorCamera.lensShift = new Vector2(-localPos.x, -localPos.y);
+        mirrorCamera.focalLength = mirrorCamera.sensorSize.x * localPos.z;//renderCam.sensorSize.x = 12.8
+        mirrorCamera.transform.localPosition = Vector3.Reflect(localPos, Vector3.forward);
+        mirrorCamera.nearClipPlane = -mirrorCamera.transform.localPosition.z * transform.localScale.x;
+    }
+}
